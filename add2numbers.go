d42fee510007d7eeae70e073bd182c9d6a9bd623
chandan_ds <chandandhanush273747@gmail.com>
2026-02-18 09:45:36 +0530

    basic programs

---
+++ add2numbers.go
@@ -0,0 +1,9 @@
+
+import "fmt"
+
+func add2numbers(a int, b int){
+	sum := a + b
+
+	fmt.Println(sum)
+
+}
---
+++ evenOrOddNumber.go
@@ -0,0 +1,11 @@
+
+
+import "fmt"
+
+func evenChecker(number int){
+	if number%2 ==0{
+		fmt.Println("The number is even ", number)
+	}else{
+	fmt.Println("The number is odd ", number)	
+	}
+}
